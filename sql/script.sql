21a825db7b713f4f701ed17eaba0fa6da58822cf
patymtz17 <181730@upslp.edu.mx>
2025-08-19 19:44:39 -0600

    feature/181730

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,11 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
+
+INSERT INTO users (name,email) VALUES
+('Alice','alice@gmail.com'),
+('Bob', 'bob@gmail.com');
+
 
